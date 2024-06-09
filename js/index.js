50b353dc32dd38cf14ecccb7994a24f4b16a70b1
Kevin <kevinstein@gmx.net>
2024-06-09 19:51:37 +0200

    add global css files

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
-const appBodyElement = document.querySelector('[data-js="appbody"]');
+/* const appBody = document.querySelector('[data-js="body"]');
 const profileCardElement = document.querySelector('[data-js="profile_card"]');
 const questionCardElement = document.querySelector('[data-="question_card"]');
-const toggleButton = document.querySelector('[data-js="toogle-button"]');
+const toggleButton = document.querySelector('.switch input[type="checkbo"]');
 
-console.log(toggleButton)
+
 
 toggleButton.addEventListener('click', () => {
-    appBodyElement.classList.toggle("dark");
-});
+
+    appBodyElement.classList.toggle("dark")
+}); */
